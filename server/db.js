9ae42001d6f0b46fdd053ecb8d8e1cd24e593fd8
jdjohar <jdeep514@gmail.com>
2024-02-24 20:17:44 +0530

    Update Db Connection

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 // const mongoURI = 'mongodb+srv://katariaesha02:esha00046@cluster0.swwooje.mongodb.net/gofoomern?retryWrites=true&w=majority';
-const mongoURI = 'mongodb+srv://bablubhown:mycabinets@cluster0.evzfn2i.mongodb.net/cabinets?retryWrites=true&w=majority';
+// const mongoURI = 'mongodb+srv://bablubhown:mycabinets@cluster0.evzfn2i.mongodb.net/cabinets?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://bablubhown:mycabinets@cluster0.iyza0fm.mongodb.net/cabinets?retryWrites=true&w=majority&appName=Cluster0';
 const mongoDB = async() => {
-    mongoose.connect(mongoURI, {useNewUrlParser: true },async (err, result) => {
+    mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true},async (err, result) => {
     if(err) console.log('Some Error -- ', err)
         else { 
              const fetch_data = await mongoose.connection.db.collection("users");
