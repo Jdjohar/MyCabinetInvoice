64ff428d123be461e0c5ba3396c33cb2efa1a3f5
jdjohar <jdeep514@gmail.com>
2024-02-24 17:23:18 +0530

    change db

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const mongoURI = 'mongodb+srv://katariaesha02:esha00046@cluster0.swwooje.mongodb.net/gofoomern?retryWrites=true&w=majority';
-const mongoURI = 'mongodb+srv://eshajdwebservices:jdweb123@cluster0.evzfn2i.mongodb.net/invoicemanagement?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://bablubhown:Mycabinets@123@cluster0.evzfn2i.mongodb.net/cabinets?retryWrites=true&w=majority';
 const mongoDB = async() => {
     mongoose.connect(mongoURI, {useNewUrlParser: true },async (err, result) => {
     if(err) console.log('Some Error -- ', err)
